df321ff0e6a1c71c9304622577022370071bcc83
wh1teR4BBIT <marcin@koperkiewicz.com>
2019-05-20 23:23:36 +0200

    Header component

---
+++ packages/web/components/Header/data.ts
@@ -0,0 +1,15 @@
+import { IHeader } from "./types";
+
+export const data: IHeader = {
+  logo: {
+    url:
+      "https://www.opole.pl/wp-content/uploads/2018/05/instagram-logo-text-black-png.png",
+    title: "Instagram",
+    href: "#"
+  },
+  elements: [
+    { text: "Najnowsze", href: "#" },
+    { text: "Mój profil", href: "#" },
+    { text: "Ustawienia", href: "#" }
+  ]
+};
