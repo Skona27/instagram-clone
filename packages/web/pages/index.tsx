05004f52517b29cfd410a02042e064ab8e51a7c6
wh1teR4BBIT <marcin@koperkiewicz.com>
2019-05-30 23:12:17 +0200

    Add missing <Footer /> in index.tsx

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 const Index = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default Index;
