b2819f1a092eaa7e9f048399c09be1ba5eb470fd
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-06-03 20:48:30 +0200

    Add ThemeContext in UI, Change usage of colors in WEB to theme

--- packages/web/components/Header/components/NavbarItem.tsx
+++ packages/web/components/Header/components/NavbarItem.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { ILink } from "../types";
+import { useTheme } from "@insta/ui";
 
 export const NavbarItem: React.FC<ILink> = React.memo(({ href, text }) => {
+  const { colors } = useTheme();
+
   return (
     <li
       css={{
@@ -14,13 +17,13 @@ export const NavbarItem: React.FC<ILink> = React.memo(({ href, text }) => {
         href={href}
         css={{
           textDecoration: "none",
-          color: "#003569",
+          color: colors.dark_blue,
           textTransform: "uppercase",
           fontWeight: 600,
           letterSpacing: 1,
           transition: "color .15s",
           ":hover": {
-            color: "#3a80c5"
+            color: colors.light_blue
           }
         }}
       >
